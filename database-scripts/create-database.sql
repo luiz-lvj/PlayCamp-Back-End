9db3a4e360e11196e428adc1ff5326369b43998b
Luiz <lvj253@gmail.com>
2021-07-11 01:18:06 -0300

    nova linha no script sql

--- database-scripts/create-database.sql
+++ database-scripts/create-database.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE playcamp;
 CREATE USER playcampadmin WITH PASSWORD '123';
 GRANT ALL PRIVILEGES ON DATABASE playcamp to playcampadmin;
+ALTER DATABASE playcamp OWNER TO playcampadmin;
