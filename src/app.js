55fa85cf63912dcc99c94ceed55ff70d9b710bdf
Daniel Luiz <danielluizthiessen@gmail.com>
2021-07-11 21:11:43 -0300

    implementei rota de criação de jogos

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { handlePostSignup } from './handlers/handleSignup.js';
 import { handlePostSignin } from './handlers/handleSignin.js';
 import { handlePostLogout } from './handlers/handleLogout.js';
+import { handleCreation } from './handlers/handleCreation.js';
 
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.post("/signup", async (req, res) => await handlePostSignup(req, res));
 app.post("/signin", async (req, res) => await handlePostSignin(req, res));
 app.post("/logout", async (req, res) => await handlePostLogout(req, res));
+app.post("/addgame", async (req, res) => await handleCreation(req, res));
 
 
 export default app;
