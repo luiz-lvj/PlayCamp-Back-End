cff915d1f63bd8ac787d2afd26c2d532c4d85fbd
Daniel Luiz <danielluizthiessen@gmail.com>
2021-07-12 02:16:06 -0300

    implementei testes

--- src/connectDb.js
+++ src/connectDb.js
@@ -2,17 +2,13 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-const user = "playcampadmin";
-const password = "123";
-const host = 'localhost';
-const port_database = 5432;
-const database = 'playcamp';
 
 const connection = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+	host: "ec2-54-227-246-76.compute-1.amazonaws.com",
+	user: "elpbxjazicbpma",
+	password: "84e60c120bcdabf2de963d06e00a19f1de9f05260af1680092d86e77006643dc",
+	port: 5432,
+	database: "dd2peo984vm03d"
 });
 
 export default connection;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from './app.js';
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port " + process.env.PORT);
+app.listen(5432, () => {
+  console.log("Server running on port 5432");
 });
