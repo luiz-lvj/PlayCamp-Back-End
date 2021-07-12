98e53a40af206e3b34174d7566204e7947565391
Daniel Luiz <danielluizthiessen@gmail.com>
2021-07-11 22:22:12 -0300

    Configurei rota get da pagina inicial de jogos

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { handlePostSignup } from './handlers/handleSignup.js';
 import { handlePostSignin } from './handlers/handleSignin.js';
 import { handlePostLogout } from './handlers/handleLogout.js';
 import { handleCreation } from './handlers/handleCreation.js';
+import { handleLoad } from './handlers/handleLoad.js';
 
 
 const app = express();
@@ -14,6 +15,7 @@ app.post("/signup", async (req, res) => await handlePostSignup(req, res));
 app.post("/signin", async (req, res) => await handlePostSignin(req, res));
 app.post("/logout", async (req, res) => await handlePostLogout(req, res));
 app.post("/addgame", async (req, res) => await handleCreation(req, res));
+app.get("/games", async (req, res) => await handleLoad(req, res));
 
 
 export default app;
